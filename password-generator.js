8d1ff95edd0075ac44be9b6882453aec3d27998c
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-10-30 21:30:43 +0100

    password-generator: Create getRandomCharacters function

--- password-generator.js
+++ password-generator.js
@@ -5,4 +5,15 @@ import {
   numbers,
 } from "./src/data.js";
 
+const getRandomCharacters = (amountCharacters, charactersArray) => {
+  let passwordLetters = [];
+
+  for (let i = 0; i < amountCharacters; i++) {
+    const randomNumber = Math.floor(Math.random() * charactersArray.length);
+    const randomCharacter = charactersArray[randomNumber];
+    passwordLetters.push(randomCharacter);
+  }
+  return passwordLetters;
+};
+console.log(getRandomCharacters(10, numbers));
 export function passwordGenerator() {}
