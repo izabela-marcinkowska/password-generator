476627414e5e2d397b18a69f3c51289047a90b90
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-11-10 08:36:39 +0100

    feat: Add getAmountChar function

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ import { passwordGenerator } from "./password-generator";
 const resultParagraph = document.getElementById("result-window");
 const resultButton = document.getElementById("result-button");
 
+const getAmountChar = () => {
+  const numChar = document.getElementById("amount").value;
+  const parsedNr = Number(numChar);
+  return parsedNr;
+};
+
 resultButton.addEventListener("click", () => {
-  resultParagraph.innerText = passwordGenerator(20, true, true, true);
+  const amountChar = getAmountChar();
+  resultParagraph.innerText = passwordGenerator(20, true, false, true);
+  console.log("amount characters is:", amountChar);
 });
